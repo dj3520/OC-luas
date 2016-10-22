a42e9fa028d0331ccd0d248e2ff401438a8004ca
dj3520 <dj3520@gmail.com>
2016-10-22 03:32:05 -0400

    Adapt into looping script
    instead of a service like I wanted
    :frowning:

--- bigreactors/simpleReactorControl.lua
+++ bigreactors/simpleReactorControl.lua
@@ -1,17 +1,12 @@
 -- Runs a BigReactors reactor based on how full it's internal battery is.
 
 local component = require("component")
-local term = require("term")
-local event = require("event")
 local reactor = component.br_reactor
-
-local debug = false
 
 -- Config settings. Feel free to change!
 local ignoreTop = 90 -- At this level, reactor will shut down and stay off until levels drop again
 
-
-local function reactAI()
+while true do
   perc = reactor.getEnergyStored() / 100000 -- Max is actually 10,000,000 but convert to percent!
   if perc > ignoreTop then
     react.setActive(false)
@@ -21,27 +16,4 @@ local function reactAI()
     react.setAllControlRodLevels(perc)
   end
   if debug == true then print("Reactor percent: "..perc) end
-end
-
-local function doloop()
-  reactorAI()
-  if running == true then 
-    event.timer(1, doloop)
-  end
-end
-
-function stop()
-  print("Simple reactor control service stopping.")
-  running = false
-  reactor.setActive(false)
-end
-
-function start()
-  running = true
-  print("Simple reactor control service running.")
-  event.timer(1, doloop)
-end
-
-function debug(setting)
-  debug = setting
 end
